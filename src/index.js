c934ca3a46512119680c8898e6de07be89cb2dc8
archdemon7 <vakylskij@gmail.com>
2022-07-29 22:00:35 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -38,7 +38,33 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let result = [];
+for (let i = 0; i < expr.length; i += 10){
+    if (expr.slice(i, i+10) == '**********'){
+        result.push([' ']);
+    } else {
+    let subResult = [];
+    for (let k = 0; k < expr.slice(i, i+10).length; k += 2){
+        if (expr.slice(i, i+10).slice(k, k+2) == '10'){
+            subResult.push('.');
+        } else if (expr.slice(i, i+10).slice(k, k+2) == '11') {
+            subResult.push('-');
+        };
+    };
+    result.push(subResult);
+};
+};
+let result2 = []
+result.forEach((el) => result2.push(el.join('')));
+let result3 = ''
+result2.forEach((el) => {
+    if (el == ' '){
+        result3 += ' ';
+    } else {
+        result3 += MORSE_TABLE[el];
+    }
+});
+return result3
 }
 
 module.exports = {
